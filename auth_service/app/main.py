b45dba5c1986928596fdcde6c014bda5debf6628
Reza Jahankohan <reza@Rezas-MacBook-Pro.local>
2024-08-05 00:36:52 +0300

    Minor changes to db and main file to fix bug

--- auth_service/app/main.py
+++ auth_service/app/main.py
@@ -3,8 +3,8 @@ from fastapi import FastAPI
 from app.database import engine, Base
 from app.routes import auth_routes
 
-Base.metadata.create_all(bind=engine)
-
 app = FastAPI()
 
-app.include_router(auth_routes.router, prefix="/auth", tags=["auth"])
+app.include_router(auth_routes.router, prefix="", tags=[""])
+
+Base.metadata.create_all(bind=engine)
