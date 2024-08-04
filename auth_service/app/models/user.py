7c1fbdeb51d3f337b7ea2802c4853f93ef360380
Reza Jahankohan <reza@Rezas-MacBook-Pro.local>
2024-08-05 00:33:36 +0300

    Update user model, added email verification

--- auth_service/app/models/user.py
+++ auth_service/app/models/user.py
@@ -1,7 +1,9 @@
 # auth_service/app/models/user.py
 
-from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy import Column, Integer, String, Boolean, DateTime
+from datetime import datetime, timedelta, timezone
 from app.database import Base
+import random
 
 class User(Base):
     __tablename__ = 'users'
@@ -11,3 +13,10 @@ class User(Base):
     hashed_password = Column(String)
     is_active = Column(Boolean, default=True)
     is_superuser = Column(Boolean, default=False)
+    verification_code = Column(String, nullable=True)
+    verification_code_expiry = Column(DateTime, nullable=True)
+
+    def set_verification_code(self):
+        code = random.randint(100000, 999999)
+        self.verification_code = str(code)
+        self.verification_code_expiry = datetime.now(timezone.utc) + timedelta(minutes=60)
